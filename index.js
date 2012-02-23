f54e77e3acbbfac4212108e278315a32d2153a64
Matjaz Gregoric <mtyaka@gmail.com>
2012-02-23 13:09:50 +0100

    Export the n/moox clients on the base module.

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ exports.nox = require('./nox.js');
 
 // mox S3 mock client
 exports.mox = require('./mox.js');
+
+// noox object storage (swift) client
+exports.noox = require('./noox.js');
+
+// moox object storage (swift) client
+exports.moox = require('./moox.js');
